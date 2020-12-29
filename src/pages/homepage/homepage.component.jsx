eda3537a8e80cf30aadd2e2abfaf217d28172024
avethe <avethedestroyer@gmail.com>
2020-12-29 14:13:20 +0200

    broken homepage to components, style menu-item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss'
+import Directory from '../../components/directory/directory.component'
 
 const Homepage = () => (
     <div className='homepage'>
-        <div className="directory-menu">
+        <Directory/>
+        {/* <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">HATS</h1>
@@ -32,7 +34,7 @@ const Homepage = () => (
                     <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
-        </div>
+        </div> */}
     </div>
 )
 
