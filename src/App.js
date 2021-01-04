827b46d2134aab3a906632160bec6e0cab5cebd4
avethe <avethedestroyer@gmail.com>
2021-01-04 13:18:11 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component' 
+
+const Hatspage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Hatspage</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Route exact path='/' component={Homepage}/>
+      <Route exact path='/hats' component={Hatspage}/>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,40 +2,12 @@ import React from 'react';
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
-const Homepage = () => (
-    <div className='homepage'>
-        <Directory/>
-        {/* <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SNEAKERS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div> <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MENS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div> <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMANS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-        </div> */}
-    </div>
-)
-
+const Homepage = (props) => {
+    console.log(props)
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
 export default Homepage;
